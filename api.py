b6518c157cd07da511b65799d142f4a9366b7fdc
Le Guellanff Quentin <quentin.le-guellanff@universite-paris-saclay.fr>
2021-09-24 16:45:06 +0200

    changement avec passage d'id dans la requete

--- api.py
+++ api.py
@@ -13,6 +13,7 @@ def get_all_books():
     with open('books.json') as json_file:
         data = json.load(json_file)  
         return jsonify(data)
+        
  
 @app.route("/api/v1/resources/books/new", methods=["POST"])
 def post_book():
@@ -24,40 +25,44 @@ def post_book():
             if localData['books'][index]['title'] == data['title']:
                 return "Error, there is already a book with this name !"
     if(isOk):
+        data['id'] = localData['books'][len(localData['books'])-1]['id']+1
         localData['books'].append(data)
     with open('books.json','w') as json_file:
         json.dump(localData,json_file,indent=4)
     
-    return jsonify(data)
+    return '201'
     
 @app.route("/api/v1/resources/books/update", methods=["PUT"])
 def update_book():
     data = request.get_json()
+    id = request.args.get('id')
     updated = False
     with open('books.json') as json_file:
         localData = json.load(json_file)
         for index in range(len(localData['books'])):  
-            if localData['books'][index]['title'] == data['title']:
+            if localData['books'][index]['id'] == int(id):
+                data['id'] = localData['books'][len(localData['books'])-1]['id']+1
                 localData['books'][index] = data
                 updated = True
     if not updated:
+        data['id'] = localData['books'][len(localData['books'])-1]['id']+1
         localData['books'].append(data)
     with open('books.json','w') as json_file:
         json.dump(localData,json_file,indent=4)
     
-    return jsonify(data)
+    return '200'
 
 @app.route("/api/v1/resources/books/delete", methods=["DELETE"])
 def delete_books():
-    data = request.get_json()
+    id = request.args.get('id')
     deleted = False
     with open('books.json') as json_file:
         localData = json.load(json_file)
         for index in range(len(localData['books'])):  
-            if localData['books'][index]['title'] == data['title']:
+            if localData['books'][index]['id'] == int(id):
                 localData['books'].pop(index)
                 with open('books.json','w') as json_file:
                     json.dump(localData,json_file,indent=4)
-                return jsonify(localData)
+                return '200'
     if not deleted:
-        return "the book you want to delete does not exist..."
+        return "the book you want to delete does not exist..."+str(id)
